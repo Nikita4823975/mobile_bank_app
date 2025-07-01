378eaa3cc1d900f2a3bf0f111e61e1f4ec8e82e1
Nikita Bukharinov <nikita.buharinov@mail.com>
2025-07-01 19:26:44 +0300

    AAAAAAAHHHHHHHHHHH

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 
 void main() {
   runApp(const MyApp());
@@ -6,19 +7,36 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+=======
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+>>>>>>> 8a74308 (AAAAAAAHHHHHHHHHHH)
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< HEAD
       title: 'Simple iOS App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Тестовый экран'),
+=======
+      title: 'Mobile Banking App Prototype',
+      home: PhoneNumberListScreen(),
+>>>>>>> 8a74308 (AAAAAAAHHHHHHHHHHH)
     );
   }
 }
 
+<<<<<<< HEAD
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -33,10 +51,54 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
+=======
+class PhoneNumberListScreen extends StatefulWidget {
+  const PhoneNumberListScreen({super.key});
+
+  @override
+  _PhoneNumberListScreenState createState() => _PhoneNumberListScreenState();
+}
+
+class _PhoneNumberListScreenState extends State<PhoneNumberListScreen> {
+  final List<String> _phoneNumbers = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPhoneNumbers();
+  }
+
+  Future<void> _fetchPhoneNumbers() async {
+    try {
+      final url = Uri.parse('http://10.0.2.2:5000/api/phone-numbers');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body) as List;
+        setState(() {
+          _phoneNumbers.clear();
+          _phoneNumbers.addAll(data.map((item) => item['phone_number']));
+          _isLoading = false;
+        });
+      } else {
+        throw Exception('Ошибка загрузки данных: ${response.statusCode}');
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Ошибка: $e')),
+      );
+    }
+  }
+>>>>>>> 8a74308 (AAAAAAAHHHHHHHHHHH)
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< HEAD
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -79,3 +141,19 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+=======
+      appBar: AppBar(title: Text('Phone Numbers')),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : _phoneNumbers.isEmpty
+              ? Center(child: Text('Нет номеров телефонов'))
+              : ListView.builder(
+                  itemCount: _phoneNumbers.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(title: Text(_phoneNumbers[index]));
+                  },
+                ),
+    );
+  }
+}
+>>>>>>> 8a74308 (AAAAAAAHHHHHHHHHHH)
